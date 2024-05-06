2054584948dbfdad6ea53c9cdd98127c82839b67
Gustavo <gustavomirandaoliveira@outlook.com>
2024-05-06 14:24:48 -0300

    Aula03 if

---
+++ JS/aula03/if.js
@@ -0,0 +1,23 @@
+let nu1 = 20;
+let nu2 = 150;
+
+// operador de comparação ==
+console.log(nu1==nu2);
+
+// operador de comparação diferente !=
+console.log(nu1 != nu2);
+
+// operador de comparação de valor e tipos===
+console.log(nu1 === nu2);
+
+console.log(nu1 > nu2);
+// false
+
+console.log(nu1 >= nu2);
+// false
+
+console.log(nu1 < nu2);
+// true
+
+console.log(nu1 <= nu2);
+// true
