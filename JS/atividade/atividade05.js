8e751d152038a23fbb3c5386b704743de0f1d2fd
Gustavo <gustavomirandaoliveira@outlook.com>
2024-05-06 17:19:41 -0300

    Atividade js Segunda

--- JS/atividade/atividade05.js
+++ JS/atividade/atividade05.js
@@ -1,1 +1,22 @@
+let idade = 15;
 
+if (idade < 18) {
+    console.log("O usuário é menor de idade");
+}
+else if (idade < 60) {
+    console.log("O usuário é idoso");
+}
+else {
+    console.log("O usuário é adulto");
+} 
+console.log("Fim");
+
+
+if (idade >= 18) {
+    console.log("Você pode dirigir");
+}
+else if (idade < 18) {
+    console.log("Você ainda não pode dirigir");
+}
+
+console.log("Fim");
