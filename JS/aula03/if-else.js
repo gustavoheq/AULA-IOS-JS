9e80d1d9c7a547785e30c20a45fc1016faad96d3
Gustavo <gustavomirandaoliveira@outlook.com>
2024-05-06 15:45:09 -0300

    Aula 03 parte2

--- JS/aula03/if-else.js
+++ JS/aula03/if-else.js
@@ -3,3 +3,26 @@ if (media >= 9.5) {
     console.log("Parabéns você se formou com honra");
 }
 console.log("Fim");
+
+let idade = 21;
+if (idade >= 18) {
+    console.log("Você pode beber");
+}
+else {
+    console.log("Você não pode beber");
+}
+console.log("Fim");
+
+let velocidade = 75;
+if (velocidade >= 80) {
+    console.log("Multado por trafegar acima do limite permitido");
+}
+else {
+    console.log("Boa Viagem");
+}
+
+if (velocidade < 40) {
+    console.log("Multado por trafegar abaixo do limite permitido");
+}
+console.log("Fim");
+
