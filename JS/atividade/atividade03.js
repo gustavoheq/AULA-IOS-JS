c2840b12cdb6856b9f3fdab4f42504f9dbe4df8b
Gustavo <gustavomirandaoliveira@outlook.com>
2024-05-07 14:40:22 -0300

    Atividade 03 exc

--- JS/atividade/atividade03.js
+++ JS/atividade/atividade03.js
@@ -1,4 +1,9 @@
-let nome = "Gustavo Henrique Miranda Oliveira";
+// Crie uma variável com o seu nome;
+// • Depois utilizando o método Replace troque o seu nome do meio pelo último sobrenome;
+// • Crie uma string para apresentar o resultado. 
+
+let nome = "Gustavo Henrique Oliveira";
+console.log(nome);
 
 let novoNome = nome.replace("Oliveira", "Miranda");
 console.log(novoNome);
