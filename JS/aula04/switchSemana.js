d8729367064a90b9fa1fa8057f926660f0e9462d
Gustavo <gustavomirandaoliveira@outlook.com>
2024-05-08 15:53:15 -0300

    Aula 04

--- JS/aula04/switchSemana.js
+++ JS/aula04/switchSemana.js
@@ -1,27 +1,27 @@
-let number = "3"
+let dia = 3
 
-switch (number){
-    case "1":
+switch (dia) {
+    case 1:
         console.log("Hoje é Domingo");
-        break;    
-    case "2":
+        break;
+    case 2:
         console.log("Hoje é Sabado!");
-        break;    
-    case "3":
-        console.log("Hoje é Segunda!");
-        break;    
-    case "4":
-        console.log("Hoje é Terça!");
         break;
-    case "5":
-        console.log("Hoje é Quarta!");
-        break;
-    case "6":
-        console.log("Hoje é Quinta!");
-        break;
-    case "7":
+    case 3:
         console.log("Hoje é Sexta!");
         break;
-        default: console.log("Não avaliado.");
+    case 4:
+        console.log("Hoje é Quinta!");
         break;
-    }
+    case 5:
+        console.log("Hoje é Quarta!");
+        break;
+    case 6:
+        console.log("Hoje é Terça!");
+        break;
+    case 7:
+        console.log("Hoje é Segunda!");
+        break;
+    default: console.log("Não avaliado.");
+        break;
+}
