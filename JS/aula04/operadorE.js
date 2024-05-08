e3dfb968ef6bbe5759c98df499da39a6e7b2728e
Gustavo <gustavomirandaoliveira@outlook.com>
2024-05-08 14:37:04 -0300

    Aula 04 Operadores &

---
+++ JS/aula04/operadorE.js
@@ -0,0 +1,17 @@
+// let media = 8;
+// let faltas = 4;
+
+// if (media >= 6 &&  faltas <= 2) {
+//     console.log("Aprovado");    
+// } else {
+//     console.log("Reprovado");
+// }
+
+let filmeBom = true;
+let companhiaBoa = true;
+
+if (filmeBom == true && companhiaBoa == true) {
+    console.log("Vou ao cinema");    
+} else {
+    console.log("Não vou ao cinema");
+}
