9185b8641390ead0d9ac27618f5973444b7d05ce
Gustavo <gustavomirandaoliveira@outlook.com>
2024-05-09 16:23:34 -0300

    Precisa corrigi atividade entrega

--- JS/aula05/for.js
+++ JS/aula05/for.js
@@ -5,6 +5,10 @@ while (contador <= 5000) {
     contador += 500    
 }
 
-for ( let i = 0 ; i <= 5000 ; i +=500 ) {
+for ( let i = 5000 ; i >= 0 ; i -=500 ) {
     console.log(i);
+}
+
+for ( let c = 0 ; c <= 5000 ; c +=500 ) {
+    console.log(c);
 }
