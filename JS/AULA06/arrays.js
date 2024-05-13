260c4ee003e917402a8441b4fd6d687ed3981e35
Kimberly Barros <kimberlyybarros@gmail.com>
2024-05-13 14:01:09 -0300

    Aula06 Arrays

---
+++ JS/AULA06/arrays.js
@@ -0,0 +1,14 @@
+// let aluno = 'Guilherme'
+// console.log(aluno);
+// aluno = 'Davi'
+// console.log(aluno);
+
+// Arrays são usados para armazenar múltiplos valores em uma única variável.
+let grupoTcc1 = ['Lucas', 'Otávio', 'Kimberly', 'Nathan', 'Victor']
+
+const grupoTcc2 = new Array ('Mariana', 'Ana', 'Fabiano', 'Guilherme', 'Davi', 'João')
+
+console.log(grupoTcc1);
+console.log(grupoTcc1[3]);
+console.log(grupoTcc2);
+console.log(grupoTcc2[2]);
