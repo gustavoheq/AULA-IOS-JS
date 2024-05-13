e7ea6960090aa658b0e6df226dcde080945106d7
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2024-05-13 16:58:28 -0300

    Atividade exc cont.

---
+++ JS/atividade/atividadeArray.js
@@ -0,0 +1,9 @@
+let jogos = ['Albion','The Duel', 'GrandChase', 'CS']
+console.log(jogos[0]);
+console.log(jogos[2]);
+
+const suco = [
+    ['Uva', 'Morango', 'Laranja'],
+    ['Limão', 'Abacaxi', 'Goiaba'],
+    []
+]
