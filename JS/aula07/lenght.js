53b4eeb3a19ab689b3bbe2a4c6414e1434f56913
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2024-05-14 16:10:44 -0300

    Atividade 07 Pt2

---
+++ JS/aula07/lenght.js
@@ -0,0 +1,14 @@
+// let nome = "Gustavo"
+
+// console.log(nome);
+// console.log(nome.length);
+// console.log(typeof nome);
+
+let nomes = ['Leonardo', 'Vitoria', 'Luiz', 'Pedro', 'Matheus', 'Evellin']
+//                0          1         2       3         4          5
+// console.log(nomes.length);
+console.log(`Olá, ${nomes[0]}! Você foi cadastrado na plataforma.`);
+
+for (let i= 0; i < nomes.length; i++) {
+console.log(`Olá, ${nomes[i]}! Você foi cadastrado na plataforma.`);   
+}
