aba9490fc9a65fec8a593cc1c6253e75c106fc7e
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2024-05-14 13:30:16 -0300

    Atividade aula 06

--- JS/atividade/atividadeArray.js
+++ JS/atividade/atividadeArray.js
@@ -5,5 +5,9 @@ console.log(jogos[2]);
 const suco = [
     ['Uva', 'Morango', 'Laranja'],
     ['Limão', 'Abacaxi', 'Goiaba'],
-    []
+    ['Guaraná', 'Maça','Caju']
 ]
+
+console.log(suco [0][0]);
+console.log(suco [1][1]);
+console.log(suco [2][2]);
