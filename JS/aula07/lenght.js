02ef14d539409d293b6ed56f7d8a946778ac93a7
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2024-05-14 16:22:26 -0300

    Aula07 Revs Lenght

--- JS/aula07/lenght.js
+++ JS/aula07/lenght.js
@@ -6,9 +6,11 @@
 
 let nomes = ['Leonardo', 'Vitoria', 'Luiz', 'Pedro', 'Matheus', 'Evellin']
 //                0          1         2       3         4          5
-// console.log(nomes.length);
-console.log(`Olá, ${nomes[0]}! Você foi cadastrado na plataforma.`);
+console.log(nomes.length);
+console.log("------- -------");
+console.log(`Olá, ${nomes[5]}! Você foi cadastrado na plataforma.`);
+console.log("------- -------");
 
-for (let i= 0; i < nomes.length; i++) {
+for (let i= 0; i <= nomes.length; i++) {
 console.log(`Olá, ${nomes[i]}! Você foi cadastrado na plataforma.`);   
 }
