d1f0c008847e91348d90c50505bf02ef6c7635ab
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2024-05-14 14:35:01 -0300

    Aula06 Exc Revs

--- JS/atividade/atividadeArray.js
+++ JS/atividade/atividadeArray.js
@@ -5,19 +5,23 @@ console.log(jogos[2]);
 console.log("-------EXC 1-------");
 
 const sucos = [
-    ['Uva', 'Morango', 'Laranja'],  //0
-    ["Limão", "Abacaxi", "Goiaba"], //1
-    [`Guaraná`, `Maça`,`Caju`]      //2
+    ['Uva', 'Morango', 'Laranja'],   //0
+    ["Limão", "Abacaxi", "Goiaba"],  //1
+    [`Guaraná`, `Maça`, `Caju`]      //2
+    //    0       1       2
 ]
 
 let suco1, suco2
+
+sucos[1][1] = 'Ketleen'
+sucos[2][0] = 'Guilherme'
+
 
 suco1 = sucos[1][1]
 suco2 = sucos[2][0]
 console.log(suco1);
 console.log(suco2);
-console.log("------- -------");
-console.log(sucos.length);
+console.log(sucos);
 console.log("------- -------");
 console.log(sucos [0][0]);
 console.log(sucos [1][1]);
@@ -26,8 +30,8 @@ console.log("-------EXC 2-------");
 
 let n1 = 5  //10
 let n2 = 10 //5
-let troca
 
+let troca
 troca = n1
 
 n1 = n2
