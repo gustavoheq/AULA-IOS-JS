f54bd0ea35f00355538460ffff9be6b29a7a5a75
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2024-05-14 14:16:48 -0300

    Atividade Array Revs

--- JS/atividade/atividadeArray.js
+++ JS/atividade/atividadeArray.js
@@ -1,13 +1,37 @@
 let jogos = ['Albion','The Duel', 'GrandChase', 'CS']
+
 console.log(jogos[0]);
 console.log(jogos[2]);
+console.log("-------EXC 1-------");
 
-const suco = [
-    ['Uva', 'Morango', 'Laranja'],
-    ['Limão', 'Abacaxi', 'Goiaba'],
-    ['Guaraná', 'Maça','Caju']
+const sucos = [
+    ['Uva', 'Morango', 'Laranja'],  //0
+    ["Limão", "Abacaxi", "Goiaba"], //1
+    [`Guaraná`, `Maça`,`Caju`]      //2
 ]
 
-console.log(suco [0][0]);
-console.log(suco [1][1]);
-console.log(suco [2][2]);
+let suco1, suco2
+
+suco1 = sucos[1][1]
+suco2 = sucos[2][0]
+console.log(suco1);
+console.log(suco2);
+console.log("------- -------");
+console.log(sucos.length);
+console.log("------- -------");
+console.log(sucos [0][0]);
+console.log(sucos [1][1]);
+console.log(sucos [2][2]);
+console.log("-------EXC 2-------");
+
+let n1 = 5  //10
+let n2 = 10 //5
+let troca
+
+troca = n1
+
+n1 = n2
+n2 = troca
+
+console.log(n1, n2);
+console.log("-------EXC 3-------");
