fa05a1f96886cf418b89c41c942beed00f58c9ec
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2024-05-14 14:51:12 -0300

    Aula07 Pt1

---
+++ JS/aula07/metodosArray.js
@@ -0,0 +1,11 @@
+let notas = [10 , 9 , 8, 7]
+console.log(notas);
+console.log(typeof notas[0]);
+
+let notasString = notas.toString()
+console.log(notasString);
+console.log(typeof notasString);
+
+// let preco = 450.2365785
+// console.log(preco.toFixed(2));
+
