b100d603f14371d5a2cd6c634792b1796bbea13a
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2024-05-15 14:29:32 -0300

    Aula 08 add objeto

--- JS/aula08/objeto.js
+++ JS/aula08/objeto.js
@@ -13,16 +13,41 @@ let aluno1 = {
 console.log(aluno1);
 console.log(aluno1.Nome);
 
-// let aluno2 = {
-//     RA: 20240102,
-//     Nome: 'Ketleen',
-//     Idade: 18,
-//     Grade: {
-//         Curso: 'Comunicação',
-//         CH: 200,
-//         Matriculado: true
-//     }
-// }
+let animal1 = {
+    Nome: 'Yasu',
+    Raça: 'Felino',
+    Idade: 1,
+    Pet: {
+        Vacinado: true,
+        Castrado: true
+    }
+}
 
-// console.log(aluno2);
-// console.log(aluno2.Nome);
+console.log(animal1);
+console.log(animal1.Nome);
+
+let animal2 = {
+    Nome: 'Yusu',
+    Raça: 'Poodle',
+    Idade: 7,
+    Pet: {
+        Vacinado: true,
+        Castrado: true
+    }
+}
+
+console.log(animal2);
+console.log(animal2.Nome);
+
+let animal3 = {
+    Nome: 'Yosu',
+    Raça: 'Chihuahua',
+    Idade: 1,
+    Pet: {
+        Vacinado: false,
+        Castrado: false
+    }
+}
+
+console.log(animal3);
+console.log(animal3.Nome);
