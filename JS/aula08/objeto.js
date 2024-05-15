fc3519ce6aa6d18ccef6fca9ef39e9ba9d9fe731
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2024-05-15 14:17:23 -0300

    Aula 08 Pt1

---
+++ JS/aula08/objeto.js
@@ -0,0 +1,28 @@
+//RA, Nome, Idade, Grade:Tecnologia, Administração, Comunicação
+let aluno1 = {
+    RA: 20240101,
+    Nome: 'Ketleen',
+    Idade: 18,
+    Grade: {
+        Curso: 'Comunicação',
+        CH: 200,
+        Matriculado: true
+    }
+}
+
+console.log(aluno1);
+console.log(aluno1.Nome);
+
+// let aluno2 = {
+//     RA: 20240102,
+//     Nome: 'Ketleen',
+//     Idade: 18,
+//     Grade: {
+//         Curso: 'Comunicação',
+//         CH: 200,
+//         Matriculado: true
+//     }
+// }
+
+// console.log(aluno2);
+// console.log(aluno2.Nome);
