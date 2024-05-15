2f1a8c9e9fd960d48a840859e05a627ff94b8999
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2024-05-15 17:15:43 -0300

    Aula08 Pt2 AtividadeJs

--- JS/aula01/main.js
+++ JS/aula01/main.js
@@ -5,7 +5,14 @@
 //Number
 // Boolean
 
-let nome = "Luis" //String
+
+
+let pet = "Hugo"
+let tipoPet = 'Cachorro'
+let idadePet = 5
+let RD = `Shih-Tzu`
+
+let nome = "Hugo" //String
 let idade = 7.8 //Number
 let praia = false // Boolean
 
@@ -13,15 +20,10 @@ let mNome = "Gustavo"
 let mIdade = 27
 let Casa = false
 
-let pet = "Hugo"
-let tipoPet = 'Cachorro'
-let idadePet = 5
-let RD = `Shih-Tzu`
-
-console.log("Olá, ", mNome, "!Tudo bem?");
-console.log("Olá, ", + mNome);
+console.log("Olá,", mNome, "!Tudo bem?");
+console.log(`Olá, ${mNome}`);
 
 
 console.log(`Olá, ${mNome}! Tudo bem?`);
-console.log(`${mNome}tem ${idade +1} anos de idade.`);
-console.log(mNome, 'tem', idade, 'anos de idade.');
+console.log(`${mNome}, tem ${mIdade}`, 'anos de idade.');
+console.log(`${mNome}, tem ${mIdade +1} anos de idade.`);
--- JS/aula05/for.js
+++ JS/aula05/for.js
@@ -4,11 +4,11 @@ while (contador <= 5000) {
     console.log(contador);
     contador += 500    
 }
-
+console.log('---- ---- ----');
 for ( let i = 5000 ; i >= 0 ; i -=500 ) {
     console.log(i);
 }
-
+console.log('---- ---- ----');
 for ( let c = 0 ; c <= 5000 ; c +=500 ) {
     console.log(c);
 }
