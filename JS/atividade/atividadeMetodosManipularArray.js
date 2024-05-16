f7130f96f61f0773d32823b742ddd6449ab49bb6
Gustavo <gustavomirandaoliveira@outlook.com>
2024-05-15 23:16:23 -0300

    Atividade Entrega pra Sexta

--- JS/atividade/atividadeMetodosManipularArray.js
+++ JS/atividade/atividadeMetodosManipularArray.js
@@ -1,15 +1,13 @@
 var clientes = [];
 
 do {
-    var nomeCliente = prompt("Por favor, insira o nome do cliente:");
+    var nomeCliente = prompt("Insira o nome do cliente:");
 
     clientes.push(nomeCliente);
 
-    var resposta = prompt("Deseja inserir mais algum nome de cliente? (sim/não)").toLowerCase();
+    var resposta = prompt("Deseja inserir mais algum nome? (sim/não)").toLowerCase();
 } while (resposta === 'sim');
 
 if (clientes.length > 0) {
     alert("Lista de clientes:\n" + clientes.join("\n"));
-} else {
-    alert("Nenhum cliente foi inserido.");
 }
