b7ac0153aa6218f8f817fce3f4318f5e549c3ca0
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2024-05-20 14:42:41 -0300

    Aula 10 JS Eventos Pt1

--- JS/aula10/eventos.js
+++ JS/aula10/eventos.js
@@ -3,5 +3,18 @@ const clique = () => {
 }
 
 const eventoH1 = () => {
-    console.log(`Evento do mouse sobre o H1`);
+    console.log(`Evento do mouse sobre o H1.`);
 }
+
+const pasMouse = () => {
+    console.log(`O usuário move o mouse sobre o elemento HTML.`);
+}
+
+const tiraMouse = () => {
+    console.log(`O usuário tira o mouse de cima do elemento HTML.`);
+}
+
+const presTecla = () => {
+    console.log(`O usuário pressiona um Tecla do teclado.`);
+}
+
