0fb886497890c79633cb991306fa4648a27f8a0e
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2024-05-22 17:31:04 -0300

    Aula11 Revs ativ4

--- JS/aula11/objeto.js
+++ JS/aula11/objeto.js
@@ -7,7 +7,7 @@ console.log(mariana);
 
 let moises = {
     marca: 'Motorola',
-    modelo: 'Motog42',
+    modelo: 'MotoG42',
     ano: 2022,
 }
 console.log(moises);
@@ -18,6 +18,7 @@ let evellin = {
     ano: 2019,
 }
 console.log(evellin);
+console.log(`--- --- ---`);
 
 class Celular {
     constructor(marca, modelo, ano) {
@@ -25,9 +26,45 @@ class Celular {
         this.modelo = modelo;
         this.ano = ano;
     }
+    desbloquearCelular() {
+        return "Celular Desbloqueado"
+    }
+    efetuarLigacao() {
+        return "Efetuando ligação"
+    }
+    desbloquearCelular2() {
+        console.log("Celular Desbloqueado");
+    }
 }
 
+class CelularGamer extends Celular {
+    constructor(marca, modelo, ano, ram, cooler) {
+        super(marca, modelo, ano);
+        this.ram = ram;
+        this.cooler = cooler;
+    }
+    ligarGameBooster(){
+        console.log("Otimiza o jogo.");
+    }
+}
 
-let kimberlly = new Celular("Samsumg", "A32", 2022)
-console.log(typeof kimberlly);
-console.log(kimberlly.modelo);
+let celGamer1 = new CelularGamer('RedMagic', '9PRO', 2025, 32, false)
+
+class CelularGamerPremium extends CelularGamer {
+    constructor(){
+        
+    }
+}
+
+// let pedro = new Celular("Samsumg", "S21", 2023)
+// console.log(pedro.desbloquearCelular());
+// console.log(pedro.efetuarLigacao());
+// console.log(`--- --- ---`);
+
+// let Leonardo = new Celular("Xiaomi", "Poco M3", 2020)
+// console.log(Leonardo);
+// console.log(`--- --- ---`);
+
+// let Kauanny = new Celular("Samsumg", "M62", 2021)
+// console.log(Kauanny);
+// console.log(`--- --- ---`);
