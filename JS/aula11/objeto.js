f9c13890c5d6b53ec1ec3f4e22f4dd4124791dc9
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2024-05-22 15:59:08 -0300

    Aula11 Atividade Pt2

---
+++ JS/aula11/objeto.js
@@ -0,0 +1,20 @@
+let mariana = {
+    marca: 'Apple',
+    modelo: 'Iphone-11',
+    ano: 2019,
+}
+console.log(mariana);
+
+let moises = {
+    marca: 'Motorola',
+    modelo: 'Motog42',
+    ano: 2022,
+}
+console.log(moises);
+
+let evellin = {
+    marca: 'Apple',
+    modelo: 'Iphone-11',
+    ano: 2019,
+}
+console.log(evellin);
