8dbd2e3ee7e0f24e50015f20339a6d33e1ce8f49
Gustavo <gustavomirandaoliveira@outlook.com>
2024-05-23 23:26:53 -0300

    Aula12 Atividade1 Revs

--- JS/aula12/atividade1.js
+++ JS/aula12/atividade1.js
@@ -1,4 +1,4 @@
-// document.title = 123;
+document.title = 123;
 
 let heading = document.createElement('H1');
 heading.innerHTML = 'Gustavo Henrique';
@@ -7,25 +7,19 @@ heading.style.border = '3px solid #000';
 heading.style.color = 'red';
 
 function alternarCorBorda(elemento) {
-
-    let corAtual = elemento.style.borderColor;
-    const coresAlternativas = ['red', 'blue']
-
-    if (corAtual === 'red') {
-        elemento.style.borderColor = 'blue';
-    } else {
-        elemento.style.borderColor = 'red';
-    }
+    const corAtual = elemento.style.borderColor;
+    const corAlternativa = (corAtual === 'red') ? 'blue' : 'red';
+    elemento.style.borderColor = corAlternativa;
 }
 
 const nomeUsuario = prompt("Por favor, digite seu nome:");
 const h1Elemento = document.createElement('h1');
 
-h1Elemento.textcontent = nomeUsuario;
+h1Elemento.textContent = nomeUsuario;
 h1Elemento.style.border = "3px solid red";
 h1Elemento.style.color = "darkblue";
 document.body.appendChild(h1Elemento);
 
-setIntervalo(() => {
+setInterval(() => {
     alternarCorBorda(h1Elemento);
 }, 1000);
