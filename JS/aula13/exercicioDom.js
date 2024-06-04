f8db6c50b620258ac766d973142ccd7690c48c31
Gustavo <gustavomirandaoliveira@outlook.com>
2024-06-04 20:47:00 -0300

    Exercicio Revs Dom

--- JS/aula13/exercicioDom.js
+++ JS/aula13/exercicioDom.js
@@ -1,28 +1,29 @@
 const classificarPeso = () => {
-    let altura = Number(document.querySelector('#altura'))
-    let peso = Number(document.querySelector('#peso'))
+    let altura = parseFloat(document.querySelector('#altura').value);
+    let peso = parseFloat(document.querySelector('#peso').value);
     
-    let imc = peso / (altura * altura)
+    let res = document.querySelector('#resultado');
 
-    let res = document.querySelector('#resultado')
-    res.innerHTML = `Seu IMC ${imc}`
+    let imc = peso / (altura * altura);
+
+    res.innerHTML = `Seu IMC: ${imc.toFixed(2)}<br>`;
 
     if (imc < 16.9) {
-        res.innerHTML = "Você está muito abaixo do peso";
+        res.innerHTML += "Você está muito abaixo do peso";
     } else if (imc >= 17 && imc <= 18.4) {
-        res.innerHTML = "Você está abaixo do peso";
+        res.innerHTML += "Você está abaixo do peso";
     } else if (imc >= 18.5 && imc <= 24.9) {
-        res.innerHTML = "Você está com peso normal";
+        res.innerHTML += "Você está com peso normal";
     } else if (imc >= 25 && imc <= 29.9) {
-        res.innerHTML = "Você está acima do peso";
+        res.innerHTML += "Você está acima do peso";
     } else if (imc >= 30 && imc <= 34.9) {
-        res.innerHTML = "Você está com obesidade grau I";
+        res.innerHTML += "Você está com obesidade grau I";
     } else if (imc >= 35 && imc <= 40) {
-        res.innerHTML = "Você está obesidade grau II";
+        res.innerHTML += "Você está com obesidade grau II";
     } else {
-        res.innerHTML = "Você está obesidade grau III";
+        res.innerHTML += "Você está com obesidade grau III";
     }
 }
 
-let calcular = document.querySelector('#botaoEnviar')
-calcular.addEventListener('click', classificarPeso)
+let calcular = document.querySelector('#botaoEnviar');
+calcular.addEventListener('click', classificarPeso);
