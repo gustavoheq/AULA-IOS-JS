a324ca277ec338331bcd771551fb90a5ec2dcc30
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2024-06-04 15:20:09 -0300

    Atividade Dom corrigida

--- JS/aula13/eventosDom.js
+++ JS/aula13/eventosDom.js
@@ -1,6 +1,6 @@
-const calcularMedia = (n1, n2) => {
-    let n1 = Number(document.querySelector('#nota1'))
-    let n2 = Number(document.querySelector('#nota2'))
+const calcularMedia = () => {
+    let n1 = Number(document.querySelector('#nota1').value)
+    let n2 = Number(document.querySelector('#nota2').value)
     let media = (n1 + n2) / 2
 
     let res = document.querySelector('#resultado')
